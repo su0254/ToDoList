29d9470b13ce83fc1a23d5828a26b0ea4b4b6144
su0254 <sarau0254@gmail.com>
2025-02-06 23:36:43 +0200

    change table name

--- TodoServer/ToDoDbContext.cs
+++ TodoServer/ToDoDbContext.cs
@@ -31,7 +31,7 @@ public partial class ToDoDbContext : DbContext
         {
             entity.HasKey(e => e.Id).HasName("PRIMARY");
 
-            entity.ToTable("Items");
+            entity.ToTable("Item");
 
             entity.Property(e => e.Name).HasMaxLength(100);
         });
